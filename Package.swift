696ecb76b32fdbb7681b9278b41faca72e6cce30
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-08-29 17:54:18 +0000

    Update 73.1.2-DEV.5

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "TruvideoSdkImage",
+    products: [
+        .library(
+            name: "TruvideoSdkImage",
+            targets: ["TruvideoSdkImageTargets"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "TruvideoSdkImage",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-image/releases/download/73.1.2-DEV.5/TruvideoSdkImage.xcframework.zip",
+            checksum: "385469cb75e60daa4825afe8f462fe4fd9aa6ceebeca93f1937329514f4fc3d3"
+        ),
+        .target(
+            name: "TruvideoSdkImageTargets",
+            dependencies: [
+                .target(name: "TruvideoSdkImage")
+            ],
+            path: "Sources"
+        )
+    ]
+)
