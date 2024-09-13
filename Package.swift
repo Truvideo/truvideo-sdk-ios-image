1f2556db6f1a14d2f90a68b75dd3b261d6d1e06d
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-09-13 18:37:59 +0000

    Update 75.1.1-BETA.13

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdkImage",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-image/releases/download/73.1.2-DEV.6/TruvideoSdkImage.xcframework.zip",
-            checksum: "974795be7a799e25b13c78297aa6540ce4385f8fbdc6c47a2158d0330649947a"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-image/releases/download/75.1.1-BETA.13/TruvideoSdkImage.xcframework.zip",
+            checksum: "7eebb2d625ef089fcc7ab673881c658ecccb1185864f91cf2b101eeec7bdf52e"
         ),
         .target(
             name: "TruvideoSdkImageTargets",
